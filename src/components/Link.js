c1dd26cee6c1e6fa1beafe31084ee443405ed5b1
Anastasiia Mishunina <nastya-art@yandex.ru>
2018-07-07 22:16:48 +0300

    init layout

---
+++ src/components/Link.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const StyledLink = styled.a`
+	color:  ${props => props.active ? 'pink' : '#fff'};
+	font-size: 14px;
+	font-weight: 300;
+	line-height: 24px;
+	text-align: right;
+	padding-right: 33px;
+	border-right: 1px dotted #fff;
+
+	&:hover{
+		text-decoration: none;
+		color: #8d8d8d;
+	}
+
+	&:last-child{
+		border-right: none;
+	}
+`
+
+class Link extends React.Component{
+	render(){
+		return (
+			<StyledLink href={this.props.link}>
+				{this.props.text}
+			</StyledLink>
+		)
+	}
+}
+
+export default Link;
---
+++ src/components/Main.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+
+
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import styled from 'styled-components';
+
+const Repair = styled.div`
+	color:  #fff;
+	font-size: 40px;
+	font-weight: 700;
+	line-height: 50px;
+
+	span{
+		font-size: 24px;
+		display: block;
+		line-height: 30px;
+	}
+`
+const LineText = styled.div`
+	color:  #ffffff;
+	font-size: 18px;
+	font-weight: 300;
+	line-height: 28px;
+	margin-top: 54px;
+`
+
+const CallBtn = styled.button`
+	border-radius: 32px;
+	background-color: #ffa14b;
+	width: 245px;
+	height: 64px;
+	color:  #ffffff;
+	font-size: 18px;
+	font-weight: 400;
+	line-height: 24px;
+	margin-top: 32px;
+`
+class Main extends React.Component{
+
+	render(){
+		return (
+			<Row>
+				<Col lg={5}>
+					<Repair>
+						Качественный ремонт
+						<span>iphone за 35 минут и гарантия 1 год</span>
+					</Repair>
+					<LineText>
+						Оставьте заявку на бесплатную диагностику без очереди,
+						и получите защитное стекло, стоимостью 1000 рублей,
+						с установкой в подарок!
+					</LineText>
+					<CallBtn>Отправить заявку!</CallBtn>
+				</Col>
+				<Col lg={6} lgOffset={1}>
+					
+				</Col>
+			</Row>
+			)
+	}
+}
+
+export default Main;
